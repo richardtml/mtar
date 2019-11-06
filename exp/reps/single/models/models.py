b344105526f41461c5e918aaaf57b55d1fe5f34c
Ricardo Montalvo Lezama <omx.mxx@gmail.com>
2019-11-06 15:15:50 -0600

    add single frame experiment

--- exp/reps/single/models/models.py
+++ exp/reps/single/models/models.py
@@ -6,3 +6,4 @@ from exp.reps.single.models.conv2d1d import Conv2D1D
 from exp.reps.single.models.fullconv import FullConv
 from exp.reps.single.models.mean import FCMean, MeanFC
 from exp.reps.single.models.rec import Rec
+from exp.reps.single.models.sframe import SFrame
