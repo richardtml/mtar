cc23dee6daaca065910759ef9fd64868f3cb7583
Ricardo Montalvo Lezama <omx.mxx@gmail.com>
2019-11-24 15:52:29 -0600

    add augmentations

--- exp/frames/multi/data/vt.py
+++ exp/frames/multi/data/vt.py
@@ -20,11 +20,63 @@ class VideoTransform:
   """Video transform for data augmentation."""
 
   def __init__(self, shape=(224, 224)):
-    sometimes = lambda aug: va.Sometimes(0.5, aug) # Used to apply augmentor with 50% probability
+    sometimes = lambda aug: va.Sometimes(0.5, aug)
     self.seq = va.Sequential([
-      va.RandomCrop(size=(240, 180)), # randomly crop video with a size of (240 x 180)
-      va.RandomRotate(degrees=10), # randomly rotates the video with a degree randomly choosen from [-10, 10]
-      sometimes(va.HorizontalFlip()) # horizontally flip the video with 50% probability
+      # Add a value to all pixel intesities in an video
+      sometimes(
+        va.OneOf([
+          va.Add(-100),
+          va.Add(100),
+          va.Add(-60),
+          va.Add(60),
+        ]),
+      ),
+      # Extract center crop of the video
+      sometimes(
+        va.OneOf([
+          va.CenterCrop((224,224)),
+          va.CenterCrop((200,200)),
+          va.CenterCrop((180,180)),
+        ]),
+      ),
+      # Augmenter that sets a certain fraction of pixel intesities to 255,
+      # hence they become white
+      sometimes(
+        va.OneOf([
+          va.Salt(50),
+          va.Salt(100),
+          va.Salt(150),
+          va.Salt(200),
+        ]),
+      ),
+      # Augmenter that sets a certain fraction of pixel intensities to 0,
+      # hence they become black
+      sometimes(
+        va.OneOf([
+          va.Pepper(50),
+          va.Pepper(100),
+          va.Pepper(150),
+          va.Pepper(200),
+        ]),
+      ),
+      # Rotate video randomly by a random angle within given bounds
+      sometimes(
+        va.OneOf([
+          va.RandomRotate(degrees=10),
+          va.RandomRotate(degrees=15),
+          va.RandomRotate(degrees=25),
+        ]),
+      ),
+      # Shifting video in X and Y coordinates
+      sometimes(
+        va.OneOf([
+          va.RandomTranslate(20,20),
+          va.RandomTranslate(10,10),
+          va.RandomTranslate(5,5)
+        ]),
+      ),
+      # Horizontally flip the video with 50% probability
+      sometimes(va.HorizontalFlip())
     ])
     self.shape = shape
 
