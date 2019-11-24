04b904fb338f9d630e0b64b452bd4f4c3d2a9be0
Ricardo Montalvo Lezama <omx.mxx@gmail.com>
2019-11-23 20:34:34 -0600

    remove intrusive code

--- exp/reps/multi/eval.py
+++ exp/reps/multi/eval.py
@@ -12,12 +12,9 @@ from os.path import exists, isdir, join
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import fire
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from sklearn.metrics import confusion_matrix
-from sklearn.utils.multiclass import unique_labels
 from tqdm import tqdm, trange
 
 import models
@@ -196,64 +193,6 @@ def eval_exp(exp_dir, batch_size=128):
     dfs.append(run_df)
     df = pd.concat(dfs, sort=False)
     df.to_csv(f'{exp_dir}/results.csv')
-
-
-def plot_confusion_matrix(y_true, y_pred, classes,
-      normalize=False, title=None, cmap=plt.cm.Blues):
-  """
-  This function prints and plots the confusion matrix.
-  Normalization can be applied by setting `normalize=True`.
-  """
-  if not title:
-    if normalize:
-      title = 'Normalized confusion matrix'
-    else:
-      title = 'Confusion matrix, without normalization'
-
-  # Compute confusion matrix
-  cm = confusion_matrix(y_true, y_pred)
-  # Only use the labels that appear in the data
-  classes = classes[unique_labels(y_true, y_pred)]
-  if normalize:
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    print("Normalized confusion matrix")
-  else:
-    print('Confusion matrix, without normalization')
-
-  print(cm)
-
-  fig, ax = plt.subplots()
-  im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-  ax.figure.colorbar(im, ax=ax)
-  # We want to show all ticks...
-  ax.set(xticks=np.arange(cm.shape[1]),
-    yticks=np.arange(cm.shape[0]),
-    # ... and label them with the respective list entries
-    xticklabels=classes, yticklabels=classes,
-    title=title,
-    ylabel='True label',
-    xlabel='Predicted label')
-
-  # Rotate the tick labels and set their alignment.
-  plt.setp(ax.get_xticklabels(),
-    rotation=45, ha="right", rotation_mode="anchor")
-
-  # Loop over data dimensions and create text annotations.
-  fmt = '.2f' if normalize else 'd'
-  thresh = cm.max() / 2.
-  for i in range(cm.shape[0]):
-    for j in range(cm.shape[1]):
-      ax.text(j, i, format(cm[i, j], fmt),
-        ha="center", va="center",
-        color="white" if cm[i, j] > thresh else "black")
-  fig.tight_layout()
-  return ax
-
-def plot_cm(run_dir, epoch):
-
-
-  plot_confusion_matrix(y_test, y_pred, classes=class_names,
-    title=f'Confusion matrix {run_dir}')
 
 if __name__ == '__main__':
   fire.Fire({
